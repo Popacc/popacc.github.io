5ed21d9c1c7465498adda7a7cfd862e092400453
Popacc <nemanjapopa@gmail.com>
2019-08-29 18:20:50 +0200

    update 2

--- polyfills-es2015.js
+++ polyfills-es2015.js
@@ -6139,8 +6139,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Nemanja\Documents\Git Projects\angular-projects\train-me-online\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\Nemanja\Documents\Git Projects\angular-projects\train-me-online\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/nemanja/Documents/angular-projects/train-me-online/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/nemanja/Documents/angular-projects/train-me-online/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
